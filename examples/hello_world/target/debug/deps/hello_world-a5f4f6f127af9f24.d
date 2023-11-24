02565790c64415a1fe2d56d7be65fb5a6fb48294
Anant Narayan <anantnrg@gmail.com>
2023-11-24 11:01:44 +0530

    example

---
+++ examples/hello_world/target/debug/deps/hello_world-a5f4f6f127af9f24.d
@@ -0,0 +1,5 @@
+/home/mik3y/projects/Repos/gleam/examples/hello_world/target/debug/deps/libhello_world-a5f4f6f127af9f24.rmeta: src/main.rs
+
+/home/mik3y/projects/Repos/gleam/examples/hello_world/target/debug/deps/hello_world-a5f4f6f127af9f24.d: src/main.rs
+
+src/main.rs:
