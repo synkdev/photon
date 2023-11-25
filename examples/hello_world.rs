f2dbdac28acf928254abf7d28678be650fdeb0e3
Anant Narayan <anantnrg@gmail.com>
2023-11-25 09:05:36 +0530

    working on splitting the structs

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -11,8 +11,31 @@ use winit::{
 async fn run() {
 	env_logger::init();
 	let event_loop = EventLoop::new();
-	let window = WindowBuilder::new().build(&event_loop).unwrap();
+	let window = WindowBuilder::new().with_title("Hello World").build(&event_loop).unwrap();
 	let mut state = WgpuState::new(window).await;
+	let surface = state.surface;
+	let device = std::sync::Arc::new(state.device);
+	let size = state.size;
+	let adapter = state.adapter;
+	let queue = std::sync::Arc::new(state.queue);
+
+	let surface_caps = surface.get_capabilities(&adapter);
+	let surface_format = surface_caps
+		.formats
+		.iter()
+		.copied()
+		.find(|f| f.is_srgb())
+		.unwrap_or(surface_caps.formats[0]);
+	let config = wgpu::SurfaceConfiguration {
+		usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+		format: surface_format,
+		width: size.width,
+		height: size.height,
+		present_mode: wgpu::PresentMode::Fifo,
+		alpha_mode: wgpu::CompositeAlphaMode::Auto,
+		view_formats: vec![],
+	};
+	surface.configure(&device, &config);
 
 	event_loop.run(move |event, _, control_flow| {
 		match event {
--- src/lib.rs
+++ src/lib.rs
@@ -28,54 +28,7 @@ pub struct Glixel {
 }
 
 impl Glixel {
-	pub async fn new(window: Window) -> Self {
-		let size = window.inner_size();
-
-		let surface = unsafe { instance.create_surface(&window) }.unwrap();
-
-		let adapter = instance
-			.request_adapter(&wgpu::RequestAdapterOptionsBase {
-				power_preference: wgpu::PowerPreference::HighPerformance,
-				force_fallback_adapter: false,
-				compatible_surface: Some(&surface),
-			})
-			.await
-			.unwrap();
-
-		let (device, queue) = adapter
-			.request_device(
-				&wgpu::DeviceDescriptor {
-					features: wgpu::Features::empty(),
-					limits: wgpu::Limits::default(),
-					label: None,
-				},
-				None,
-			)
-			.await
-			.unwrap();
-
-		let surface_caps = surface.get_capabilities(&adapter);
-		let surface_format = surface_caps
-			.formats
-			.iter()
-			.copied()
-			.find(|f| f.is_srgb())
-			.unwrap_or(surface_caps.formats[0]);
-		let config = wgpu::SurfaceConfiguration {
-			usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-			format: surface_format,
-			width: size.width,
-			height: size.height,
-			present_mode: surface_caps.present_modes[0],
-			alpha_mode: surface_caps.alpha_modes[0],
-			view_formats: vec![],
-		};
-		surface.configure(&device, &config);
-
-		let clear_color = wgpu::Color::BLACK;
-
-		Self { window, surface, device, queue, config, size, instance, adapter, clear_color }
-	}
+	pub async fn new(window: Window) -> Self {}
 
 	pub fn window(&self) -> &Window {
 		&self.window
